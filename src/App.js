b7d04eae9b824749afb93040f9b91bb63ab75cac
Callum Alton <callum.alton33@gmail.com>
2020-05-21 17:18:16 +0100

    state react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
+import { render } from '@testing-library/react';
+import Person from './Components/Person'
+class App extends Component {
+  state = {
+    numbers: [1, 2, 3, 4, 5, 6, 7],
+    currentNumber: ""
+  }
+  saveNumberHandler = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      currentNumber: event.target.value
+    })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  /**
+   * log will display the current and most up to date state.
+   * updating the currentNumber to whatver event.target.value is
+   */
+  addNumberHandler = () => {
+    this.setState({
+      numbers: [...this.state.numbers, this.state.currentNumber],
+      currentNumber: ""
+    })
+  }
+  /**
+   * ... spread operator - is taking value and adding it on 
+   */
+  render() {
+    const eachNumber = this.state.numbers.map((number, index) => {
+      return <li key={index}>{number}</li>
+    })
+    return (
+      <div className="App" >
+        <ul>
+          {eachNumber}
+        </ul>
+        <input type='number' onChange={this.saveNumberHandler} value={this.state.currentNumber} />
+        <button onClick={this.addNumberHandler}>Add numbers with this button</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+export default App
---
+++ src/Components/Person.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Person = (props) => {
+    return (
+        <h1>My name is {props.name}</h1>
+
+    )
+}
+
+export default Person
